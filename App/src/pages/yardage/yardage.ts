0b395d9c1223f91b512c306c7fcc20e756e3a2ba
Matthew Behrens <mattbehrens96@gmail.com>
2016-10-12 16:33:22 -0500

    added basic form for yardage calculation, doesnt actually do anything yet

---
+++ App/src/pages/yardage/yardage.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-yardage',
+  templateUrl: 'yardage.html'
+})
+export class YardagePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
